07bdd4468e4c1920f8131609e03f40957109a5a9
Kai Walter <kai.walter@outlook.de>
2021-12-18 20:14:36 +0000

    test with Function App

---
+++ ca-pulumi-dotnet/FunctionAppStack.cs
@@ -0,0 +1,129 @@
+using Pulumi;
+using Pulumi.AzureNative.ContainerRegistry;
+using Pulumi.AzureNative.ContainerRegistry.Inputs;
+using Pulumi.AzureNative.OperationalInsights;
+using Pulumi.AzureNative.OperationalInsights.Inputs;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Web.V20210301;
+using Pulumi.AzureNative.Web.V20210301.Inputs;
+using Pulumi.Docker;
+using ContainerArgs = Pulumi.AzureNative.Web.V20210301.Inputs.ContainerArgs;
+using SecretArgs = Pulumi.AzureNative.Web.V20210301.Inputs.SecretArgs;
+
+class FunctionAppStack : Stack
+{
+    public FunctionAppStack()
+    {
+        var resourceGroup = new ResourceGroup("rg");
+
+        var workspace = new Workspace("loganalytics", new WorkspaceArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new WorkspaceSkuArgs { Name = "PerGB2018" },
+            RetentionInDays = 30,
+        });
+
+        var workspaceSharedKeys = Output.Tuple(resourceGroup.Name, workspace.Name).Apply(items =>
+            GetSharedKeys.InvokeAsync(new GetSharedKeysArgs
+            {
+                ResourceGroupName = items.Item1,
+                WorkspaceName = items.Item2,
+            }));
+
+        var kubeEnv = new KubeEnvironment("env", new KubeEnvironmentArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Type = "Managed",
+            AppLogsConfiguration = new AppLogsConfigurationArgs
+            {
+                Destination = "log-analytics",
+                LogAnalyticsConfiguration = new LogAnalyticsConfigurationArgs
+                {
+                    CustomerId = workspace.CustomerId,
+                    SharedKey = workspaceSharedKeys.Apply(r => r.PrimarySharedKey)
+                }
+            }
+        });
+
+        var registry = new Registry("registry", new RegistryArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new SkuArgs { Name = "Basic" },
+            AdminUserEnabled = true
+        });
+
+        var credentials = Output.Tuple(resourceGroup.Name, registry.Name).Apply(items =>
+            ListRegistryCredentials.InvokeAsync(new ListRegistryCredentialsArgs
+            {
+                ResourceGroupName = items.Item1,
+                RegistryName = items.Item2
+            }));
+        var adminUsername = credentials.Apply(credentials => credentials.Username);
+        var adminPassword = credentials.Apply(credentials => credentials.Passwords[0].Value);
+
+        var fapp1Name = "fapp1";
+        var fapp1Image = new Image(fapp1Name, new ImageArgs
+        {
+            ImageName = Output.Format($"{registry.LoginServer}/{fapp1Name}:v1.0.0"),
+            Build = new DockerBuild { Context = $"../{fapp1Name}" },
+            Registry = new ImageRegistry
+            {
+                Server = registry.LoginServer,
+                Username = adminUsername,
+                Password = adminPassword
+            }
+        });
+
+        var containerApp = new ContainerApp(fapp1Name, new ContainerAppArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            KubeEnvironmentId = kubeEnv.Id,
+            Configuration = new ConfigurationArgs
+            {
+                Ingress = new IngressArgs
+                {
+                    External = true,
+                    TargetPort = 80
+                },
+                Registries =
+                {
+                    new RegistryCredentialsArgs
+                    {
+                        Server = registry.LoginServer,
+                        Username = adminUsername,
+                        PasswordSecretRef = "pwd",
+                    }
+                },
+                Secrets =
+                {
+                    new SecretArgs
+                    {
+                        Name = "pwd",
+                        Value = adminPassword
+                    }
+                },
+            },
+            Template = new TemplateArgs
+            {
+                Containers =
+                {
+                    new ContainerArgs
+                    {
+                        Name = fapp1Name,
+                        Image = fapp1Image.ImageName,
+                    }
+                },
+                Scale = new ScaleArgs
+                {
+                    MaxReplicas = 3,
+                    MinReplicas = 1,
+                },
+            }
+        });
+
+        this.Url = Output.Format($"https://{containerApp.Configuration.Apply(c => c.Ingress).Apply(i => i.Fqdn)}");
+    }
+
+    [Output("url")]
+    public Output<string> Url { get; set; }
+}
--- ca-pulumi-dotnet/Program.cs
+++ ca-pulumi-dotnet/Program.cs
@@ -3,5 +3,5 @@ using Pulumi;
 
 class Program
 {
-    static Task<int> Main() => Deployment.RunAsync<DaprStack>();
+    static Task<int> Main() => Deployment.RunAsync<FunctionAppStack>();
 }
